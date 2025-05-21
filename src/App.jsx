018ddc6cdf29aee8acbb4e7f2d3d585a191648d8
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-05-21 10:05:55 +0300

    chore: install chadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-function App() {
+import { Button } from "@/components/ui/button"
 
+function App() {
   return (
-    <>
-    <h1>Movies</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
 export default App
+
