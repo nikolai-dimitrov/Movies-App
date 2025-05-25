02b333ca183bf9786bb9c8323badf33e1d78d420
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-05-25 12:19:15 +0300

    feat: implement custom debounce hook functionality

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+export const useDebounce = (currentValue, delay) => {
+	const [debounceValue, setDebounceValue] = useState(currentValue);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setDebounceValue(currentValue);
+		}, delay);
+
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [currentValue]);
+};
